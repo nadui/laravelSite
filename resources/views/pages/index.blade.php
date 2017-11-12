4a1675802fbe0a8bff8fde9e8bae0aa132a74e13
Unknown <derwishe2000@hotmail.com>
2017-11-12 22:24:05 +0100

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+       <div class="jumbotron text-center">
+            <h1> Welcome to Laravel!</h1>
+            <p>This is the SOFTWARE PROJECT WIP PROGRAM TOOL homepage.</p>
+            <p><a class ="btn btn-primary btn-lg" href="/login" role="button">Login</a></p>
+ @endsection 
